feb20a560e4c6ddf3603a00406e40612ff4ab9e5
BryanPorter <bpbryanwithay7@gmail.com>
2016-02-21 15:24:12 -0700

    Initial Commit

---
+++ Song Sampler/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Song Sampler
+//
+//  Created by Bryan Porter on 2/21/16.
+//  Copyright © 2016 Bryan Porter. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
